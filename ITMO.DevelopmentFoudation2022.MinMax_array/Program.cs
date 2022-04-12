01746827850032ab6b7150bc15f96143ddcb03ef
kanik <kanik@DESKTOP-7QGSKNO>
2022-04-12 12:23:58 +0300

    Добавьте файлы проекта.

---
+++ ITMO.DevelopmentFoudation2022.MinMax_array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.DevelopmentFoudation2022.MinMax_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] M = new int[] { 11, 4, 34, 10, 19, 7, 9, 26 };
+            int Min = M[1];
+            int Max = M[1];
+            for (int i = 2; i <= M.Length - 1; i ++)
+            {
+                if(Max < M[i])
+                {
+                    Max = M[i];
+                }
+                if(Min > M[i])
+                {
+                    Min = M[i];
+                }
+            }
+            Console.WriteLine(Max);
+            Console.WriteLine(Min);
+        }
+    }
+}
